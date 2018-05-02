0da8b48fe283fba18b788934c020cea94a2ab4ec
unknown <mhurley@hurley-consulting.io>
2018-05-02 16:45:51 -0400

    first version

---
+++ Grades/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float d = 91.5f;
+            
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+
+            GradeBook book2 = book;
+            book2.AddGrade(75);
+      
+        }
+    }
+}
