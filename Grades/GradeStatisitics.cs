c9f03488be4837def4406909063e7595bede85fe
unknown <mhurley@hurley-consulting.io>
2018-05-03 14:07:39 -0400

    Created gradebook and statistics

---
+++ Grades/GradeStatisitics.cs
@@ -0,0 +1,17 @@
+﻿namespace Grades
+{
+    class GradeStatistics
+    {
+        public GradeStatistics()
+        {
+            LowestGrade = 0;
+            HighestGrade = float.MaxValue;
+        }
+
+     public float AverageGrade;
+     public float HighestGrade;
+     public float LowestGrade;
+    }
+
+
+}
--- Grades/Program.cs
+++ Grades/Program.cs
@@ -1,18 +1,23 @@
-﻿namespace Grades
+﻿using System;
+
+namespace Grades
 {
     class Program
     {
         static void Main(string[] args)
         {
-            float d = 91.5f;
             
             GradeBook book = new GradeBook();
             book.AddGrade(91);
             book.AddGrade(89.5f);
+            book.AddGrade(75);
 
-            GradeBook book2 = book;
-            book2.AddGrade(75);
-      
-        }
+
+            GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.LowestGrade);
+
+;        }
     }
 }
