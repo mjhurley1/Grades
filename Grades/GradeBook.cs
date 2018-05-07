484a6bd774fc3547d73e2f9cd6bc786154faa737
unknown <mhurley@hurley-consulting.io>
2018-05-07 10:44:39 -0400

    Changed Geade Book

--- Grades/GradeBook.cs
+++ Grades/GradeBook.cs
@@ -31,7 +31,7 @@ namespace Grades
         {
             grades.Add(grade);
         }
-
+        public string Name;
         private List<float> grades;
     }
 }
--- Grades/Program.cs
+++ Grades/Program.cs
@@ -7,9 +7,16 @@ namespace Grades
     {
         static void Main(string[] args)
         {
-            SpeechSynthesizer synth = new SpeechSynthesizer();
-            synth.Speak("Hello, This is the grade book program");
 
+            GradeBook g1 = new GradeBook();
+            GradeBook g2 = g1;
+
+            g1.Name = "Mike's grade book";
+            Console.WriteLine(g2.Name);
+
+            
+
+            /*
             GradeBook book = new GradeBook();
             book.AddGrade(91);
             book.AddGrade(89.5f);
@@ -20,7 +27,7 @@ namespace Grades
             Console.WriteLine(stats.AverageGrade);
             Console.WriteLine(stats.HighestGrade);
             Console.WriteLine(stats.LowestGrade);
-
+            */
 ;        }
     }
 }
